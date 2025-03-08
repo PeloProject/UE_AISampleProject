0806e0f018ea962cbc2a59fb6eebf05607021c16
Wakame <peko.12.31.love.4.03@gmail.com>
2025-03-08 23:19:20 +0900

    Add Project

---
+++ Source/AISample/AISampleGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AISampleGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AAISampleGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AAISampleGameMode();
+};
+
+
+
